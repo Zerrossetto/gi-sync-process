322c343cdaf5fd90255dedcaece42d5cf4f7a8e5
Zerrossetto <tamaghei@gmail.com>
2017-10-29 19:25:49 +0100

    Refactored enumeration mapping

--- gestionaleimmobiliare/sync_agenzia/mapping/info_inserite.py
+++ gestionaleimmobiliare/sync_agenzia/mapping/info_inserite.py
@@ -1,107 +1,293 @@
-reference_id_map = {
-    1: 'bagni',
-    2: 'camere',
-    3: 'cucina',
-    4: 'soggiorno',
-    5: 'garage',
-    6: 'asta',
-    7: 'ripostigli',
-    8: 'cantina',
-    9: 'vendita',
-    10: 'affitto',
-    11: 'mansarda',
-    12: 'tavena',
-    13: 'ascensore',
-    14: 'aria_condizionata',
-    15: 'arredo',
-    16: 'riscaldamento_autonomo',
-    17: 'giardino',
-    18: 'ingresso_indipendente',
-    19: 'garage_doppio',
-    20: 'posto_auto',
-    21: 'riscaldamento_a_pavimento',
-    22: 'soggiorno_con_angolo_cottura',
-    23: 'allarme',
-    24: 'terrazzi',
-    25: 'poggioli',
-    26: 'lavanderia',
-    27: 'piano_interrato',
-    28: 'piano_terra',
-    29: 'primo_piano',
-    30: 'piano_intermedio',
-    31: 'ultimo_piano',
-    32: 'totale_piani',
-    33: 'piano_numero',
-    34: 'riscaldamento_centralizzato',
-    35: 'mare',
-    36: 'montagna',
-    37: 'lago',
-    38: 'terme',
-    39: 'collina',
-    40: 'campagna',
-    41: 'nuovo',
-    42: 'immobile_di_prestigio',
-    43: 'giardino_condominiale',
-    44: 'soffitta',
-    45: 'grezzo',
-    46: 'camino',
-    47: 'predisposizione_aria_condizionata',
-    48: 'predisposizione_allarme',
-    49: 'pannelli_solari',
-    50: 'pannelli_fotovoltaici',
-    51: 'impianto_geotermico',
-    52: 'aree_esterne',
-    53: 'ribalte',
-    54: 'urbanizzato',
-    55: 'classe_energetica',
-    56: 'posizione',
-    57: 'stato_manutenzione',
-    58: 'numero_vetrine',
-    59: 'carro_ponte',
-    60: 'impianto_anti_incendio',
-    61: 'cabina_elettrica',
-    62: 'panorama',
-    63: 'piano_semi_interrato',
-    64: 'piano_rialzato',
-    65: 'numero_locali',
-    66: 'piscina',
-    67: 'porticato',
-    68: 'soppalco',
-    69: 'sottotetto',
-    70: 'chiavi_in_agenzia',
-    71: 'accesso_disabili',
-    72: 'area_fitness',
-    73: 'frigorifero',
-    74: 'lavatrice',
-    75: 'lavastoviglie',
-    76: 'posto_spiaggia',
-    77: 'cassaforte',
-    78: 'animali_ammessi',
-    79: 'televisore',
-    80: 'forno',
-    81: 'vasca_idromassaggio',
-    82: 'caldaia_a_condensazione',
-    83: 'riscaldamento_semi_autonomo',
-    84: 'riscaldamento_termopompa',
-    85: 'raffreddamento',
-    86: 'cucina_arredata',
-    87: 'portineria',
-    88: 'domotica',
-    89: 'tapparelle_motorizzate',
-    90: 'porta_blindata',
-    91: 'contacalorie',
-    92: 'montacarichi',
-    93: 'banchine_di_carico',
-    94: 'numero_portoni',
-    95: 'numero_accessi_carrai',
-    96: 'cartello',
-    97: 'saracinesche',
-    98: 'vasca',
-    99: 'zanzariere',
-    100: 'tende_da_sole',
-    101: 'impianto_elettrico',
-    102: 'allacciamento_fognatura',
-    103: 'canna_fumaria',
-    104: 'connettivita',
-    105: 'impianto_illuminazione'
-}
+import enum
+from typing import Any
+
+
+@enum.unique
+class Tag(enum.Enum):
+
+    room = 1
+    sell_type = 2
+    furniture = 3
+    location = 4
+
+
+@enum.unique
+class EnergyLabel(enum.Enum):
+
+    da_definire = 0
+    A_plus_passiva = 1
+    A = 2
+    B = 3
+    C = 4
+    D = 5
+    E = 6
+    F = 7
+    G = 8
+    non_soggetto_certificazione = 9
+    A4 = 10
+    A3 = 11
+    A2 = 12
+    A1 = 13
+
+    @property
+    def is_ape_2015(self):
+        return self.value > 9
+
+    @staticmethod
+    def keep_in_list(value) -> bool:
+        return True
+
+
+@enum.unique
+class Location(enum.Enum):
+
+    sconosciuta = 0
+    area_industriale_artigianale = 1
+    centro_commerciale = 2
+    ad_angolo = 3
+    centrale = 4
+    servita = 5
+    forte_passaggio = 6
+    fronte_lago = 7
+    fronte_strada = 8
+    interna = 9
+
+    @staticmethod
+    def keep_in_list(value) -> bool:
+        return True
+
+
+@enum.unique
+class ManteinanceLevel(enum.Enum):
+
+    sconosciuto = 0
+    da_ristrutturare = 1
+    ristrutturato = 2
+    discreto = 3
+    buono = 4
+    ottimo = 5
+    nuovo = 6
+    impianti_da_fare = 7
+    impianti_da_rifare = 8
+    impianti_a_norma = 9
+
+    @staticmethod
+    def keep_in_list(value) -> bool:
+        return bool(value)
+
+
+@enum.unique
+class Panorama(enum.Enum):
+
+    non_indicato = 0
+    vista_mare = 1
+    vista_lago = 2
+    vista_monti = 3
+    vista_aperta = 4
+    vista_monumento = 5
+    vista_giardino = 6
+    fronte_mare = 7
+    lato_mare = 8
+
+    @staticmethod
+    def keep_in_list(value) -> bool:
+        return bool(value)
+
+@enum.unique
+class Sign(enum.Enum):
+
+    no = 0
+    si = 1
+    rimosso = 2
+    da_rimuovere = 3
+
+    @staticmethod
+    def keep_in_list(value) -> bool:
+        return True
+
+
+@enum.unique
+class Awnings(enum.Enum):
+
+    no = 0
+    si = 1
+    predisposto = 2
+
+    @staticmethod
+    def keep_in_list(value) -> bool:
+        return bool(value)
+
+
+@enum.unique
+class ElectricalSystem(enum.Enum):
+
+    non_definito = 0
+    da_fare = 1
+    a_norma = 2
+    da_verificare = 3
+
+    @staticmethod
+    def keep_in_list(value) -> bool:
+        return bool(value)
+
+
+@enum.unique
+class Connectivity(enum.Enum):
+
+    nessuna = 0
+    adsl = 1
+    fibra = 2
+
+    @staticmethod
+    def keep_in_list(value) -> bool:
+        return True
+
+
+repr_format = '<{0:}.{1:}: {2:d}, type={3:} tags={4:}>'
+
+@enum.unique
+class InfoInserita(enum.Enum):
+
+    bagni = (Tag.room,)
+    camere = (Tag.room,)
+    cucina = (Tag.room,), bool
+    soggiorno = (Tag.room,), bool
+    garage = (Tag.room,), bool
+    asta = (), bool
+    ripostigli = ()
+    cantina = (Tag.room,), bool
+    vendita = (Tag.sell_type,), bool
+    affitto = (Tag.sell_type,), bool
+    mansarda = (Tag.room,), bool
+    tavena = (), bool
+    ascensore = (), bool
+    aria_condizionata = (), bool
+    arredo = (), bool
+    riscaldamento_autonomo = (), bool
+    giardino = (), bool
+    ingresso_indipendente = (), bool
+    garage_doppio = (), bool
+    posto_auto = (), bool
+    riscaldamento_a_pavimento = (), bool
+    soggiorno_con_angolo_cottura = (), bool
+    allarme = (), bool
+    terrazzi = ()
+    poggioli = ()
+    lavanderia = (), bool
+    piano_interrato = (), bool
+    piano_terra = (), bool
+    primo_piano = (), bool
+    piano_intermedio = (), bool
+    ultimo_piano = (), bool
+    totale_piani = ()
+    piano_numero = ()
+    riscaldamento_centralizzato = (), bool
+    mare = (Tag.location,), bool
+    montagna = (Tag.location,), bool
+    lago = (Tag.location,), bool
+    terme = (Tag.location,), bool
+    collina = (Tag.location,), bool
+    campagna = (Tag.location,), bool
+    nuovo = (), bool
+    immobile_di_prestigio = (), bool
+    giardino_condominiale = (), bool
+    soffitta = (), bool
+    grezzo = (), bool
+    camino = (), bool
+    predisposizione_aria_condizionata = (), bool
+    predisposizione_allarme = (), bool
+    pannelli_solari = (), bool
+    pannelli_fotovoltaici = (), bool
+    impianto_geotermico = (), bool
+    aree_esterne = (), bool
+    ribalte = (), bool
+    urbanizzato = (), bool
+    classe_energetica = (), EnergyLabel
+    posizione = (), Location
+    stato_manutenzione = (), ManteinanceLevel
+    numero_vetrine = ()
+    carro_ponte = (), bool
+    impianto_anti_incendio = (), bool
+    cabina_elettrica = (), bool
+    panorama = (), Panorama
+    piano_semi_interrato = (), bool
+    piano_rialzato = (), bool
+    numero_locali = (), bool
+    piscina = (), bool
+    porticato = (), bool
+    soppalco = (), bool
+    sottotetto = (), bool
+    chiavi_in_agenzia = (), bool
+    accesso_disabili = (), bool
+    area_fitness = (), bool
+    frigorifero = (Tag.furniture,), bool
+    lavatrice = (Tag.furniture,), bool
+    lavastoviglie = (Tag.furniture,), bool
+    posto_spiaggia = (), bool
+    cassaforte = (Tag.furniture,), bool
+    animali_ammessi = (), bool
+    televisore = (Tag.furniture,), bool
+    forno = (Tag.furniture,), bool
+    vasca_idromassaggio = (Tag.furniture,), bool
+    caldaia_a_condensazione = (), bool
+    riscaldamento_semi_autonomo = (), bool
+    riscaldamento_termopompa = (), bool
+    raffreddamento = (), bool
+    cucina_arredata = (), bool
+    portineria = (), bool
+    domotica = (), bool
+    tapparelle_motorizzate = (), bool
+    porta_blindata = (), bool
+    contacalorie = (), bool
+    montacarichi = ()
+    banchine_di_carico = ()
+    numero_portoni = ()
+    numero_accessi_carrai = ()
+    cartello = (), Sign
+    saracinesche = ()
+    vasca = (Tag.furniture,), bool
+    zanzariere = (), bool
+    tende_da_sole = (Tag.furniture,), Awnings
+    impianto_elettrico = ()
+    allacciamento_fognatura = (), bool
+    canna_fumaria = (), bool
+    connettivita = ()
+    impianto_illuminazione = (), bool
+
+    def __new__(cls, *args):
+        obj = object.__new__(cls)
+        obj._value_ = len(cls.__members__) + 1
+        return obj
+
+    def __init__(self, *args):
+        try:
+            self.tags = args[0]
+        except IndexError:
+            self.tags = ()
+        try:
+            self.value_type = args[1]
+        except IndexError:
+            self.value_type = int
+
+    @property
+    def is_enum(self) -> bool:
+        return issubclass(self.value_type, enum.Enum)
+
+    def keep_in_list(self, value) -> bool:
+        if self.is_enum:
+            return self.value_type.keep_in_list(value)
+        else:
+            return bool(value)
+
+    def mapped_value(self, reference_value: int) -> Any:
+        return self.value_type(reference_value)
+
+    def __repr__(self):
+        return self.name
+
+    def __str__(self):
+        return repr_format.format(self.__class__.__name__,
+                                  self.name,
+                                  self.value,
+                                  self.value_type.__name__,
+                                  self.tags)
